a41e8a13d74f89ce49d37371cb6939dc3d1f9d40
gonggu <howareyou_mm@163.com>
2022-05-18 21:54:04 +0800

    帧缓存v0.1版

---
+++ StencilTest/StencilTest.cpp
@@ -0,0 +1,236 @@
+// 引入GLEW库 定义静态链接
+#define GLEW_STATIC
+#include <GLEW/glew.h>
+// 引入GLFW库
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <vector>
+
+// 包含着色器加载库
+#include "shader.h"
+#include "texture.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
+// 键盘回调函数原型声明
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+// 定义程序常量
+const int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
+
+int main(int argc, char** argv)
+{
+	
+	if (!glfwInit())	// 初始化glfw库
+	{
+		std::cout << "Error::GLFW could not initialize GLFW!" << std::endl;
+		return -1;
+	}
+
+	// 开启OpenGL 3.3 core profile
+	std::cout << "Start OpenGL core profile version 3.3" << std::endl;
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// 创建窗口
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+		"Demo of triangle", NULL, NULL);
+	if (!window)
+	{
+		std::cout << "Error::GLFW could not create winddow!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// 创建的窗口的context指定为当前context
+	glfwMakeContextCurrent(window);
+
+	// 注册窗口键盘事件回调函数
+	glfwSetKeyCallback(window, key_callback);
+
+	// 初始化GLEW 获取OpenGL函数
+	glewExperimental = GL_TRUE; // 让glew获取所有拓展函数
+	GLenum status = glewInit();
+	if (status != GLEW_OK)
+	{
+		std::cout << "Error::GLEW glew version:" << glewGetString(GLEW_VERSION) 
+			<< " error string:" << glewGetErrorString(status) << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// 设置视口参数
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	// Section1 准备顶点数据
+	// 指定顶点属性数据 顶点位置
+	GLfloat cubeVertices[] = {
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,	// A
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,	// B
+		0.5f, 0.5f, 0.5f,1.0f, 1.0f,	// C
+		0.5f, 0.5f, 0.5f,1.0f, 1.0f,	// C
+		-0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// D
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+
+
+		-0.5f, -0.5f, -0.5f,0.0f, 0.0f,	// E
+		-0.5f, 0.5f, -0.5f,0.0, 1.0f,   // H
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// F
+		-0.5f, -0.5f, -0.5f,0.0f, 0.0f,	// E
+
+		-0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// D
+		-0.5f, 0.5f, -0.5f,1.0, 1.0f,   // H
+		-0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// E
+		-0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// E
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+		-0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// D
+
+		0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// F
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// C
+		0.5f, 0.5f, 0.5f,0.0f, 1.0f,	// C
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f,	// B
+		0.5f, -0.5f, -0.5f,1.0f, 0.0f,	// F
+
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+		-0.5f, 0.5f, -0.5f,0.0, 1.0f,   // H
+		-0.5f, 0.5f, 0.5f,0.0f, 0.0f,	// D
+		-0.5f, 0.5f, 0.5f,0.0f, 0.0f,	// D
+		0.5f, 0.5f, 0.5f,1.0f, 0.0f,	// C
+		0.5f, 0.5f, -0.5f,1.0f, 1.0f,	// G
+
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,// E
+		0.5f, -0.5f, -0.5f,1.0f, 1.0f,	// F
+		0.5f, -0.5f, -0.5f,1.0f, 1.0f,	// F
+		0.5f, -0.5f, 0.5f,1.0f, 0.0f,	// B
+		-0.5f, -0.5f, 0.5f,0.0f, 0.0f,	// A
+	};
+
+		// 模板矩形
+	GLfloat stencilVertices[] = {
+		-0.5f, -0.5f, 0.5f, // A
+		-0.5f, 0.5f, 0.5f,	// B
+		-1.5f, 0.5f, 0.5f,  // C
+
+		-1.5f, 0.5f, 0.5f,  // C
+		-1.5f, -0.5f, 0.5f, // D
+		-0.5f, -0.5f, 0.5f  // A
+	};
+	// 创建缓存对象
+	GLuint stencilVAOId, stencilVBOId;
+	// Step1: 创建并绑定VAO对象
+	glGenVertexArrays(1, &stencilVAOId);
+	glBindVertexArray(stencilVAOId);
+	// Step2: 创建并绑定VBO对象
+	glGenBuffers(1, &stencilVBOId);
+	glBindBuffer(GL_ARRAY_BUFFER, stencilVBOId);
+	// Step3: 分配空间 传送数据
+	glBufferData(GL_ARRAY_BUFFER, sizeof(stencilVertices), stencilVertices, GL_STATIC_DRAW);
+	// Step4: 指定解析方式  并启用顶点属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+
+	GLuint VAOId, VBOId;
+	// Step1: 创建并绑定VAO对象
+	glGenVertexArrays(1, &VAOId);
+	glBindVertexArray(VAOId);
+	// Step2: 创建并绑定VBO对象
+	glGenBuffers(1, &VBOId);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOId);
+	// Step3: 分配空间 传送数据
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+	// Step4: 指定解析方式  并启用顶点属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	GLuint textureId = TextureHelper::load2DTexture("../resources/textures/bricks2.jpg");
+	// Section2 准备着色器程序
+	Shader stencilShader("singleColor.vertex", "singleColor.frag");
+	Shader shader("cube.vertex", "cube.frag");
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+	glEnable(GL_STENCIL_TEST);
+	// 开始游戏主循环
+	
+	glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.f, -1.f), glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)WINDOW_WIDTH / WINDOW_HEIGHT, 0.1f, 1000.0f);
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents(); // 处理例如鼠标 键盘等事件
+
+		// 清除颜色缓冲区 重置为指定颜色
+		glClearColor(0.18f, 0.04f, 0.14f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+		glm::mat4 model;
+		//1.绘制模板
+		glColorMask(GL_FALSE, GL_FALSE, GL_FALSE,GL_FALSE);// 禁止向颜色缓冲区中写颜色=》导致颜色缓冲区为黑色
+		glDepthMask(GL_FALSE);//禁止向深度缓冲区中写深度值。
+
+		glStencilMask(0xff);
+		glStencilFunc(GL_ALWAYS, 1, 0xff);
+		glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+
+		
+		// 这里填写场景绘制代码
+		glBindVertexArray(stencilVAOId);
+		stencilShader.use();
+		
+		glUniformMatrix4fv(glGetUniformLocation(stencilShader.programId, "model"),1, GL_FALSE,glm::value_ptr(model));
+		glUniformMatrix4fv(glGetUniformLocation(stencilShader.programId, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(stencilShader.programId, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);// 禁止向颜色缓冲区中写颜色=》导致颜色缓冲区为黑色
+		glDepthMask(GL_TRUE);//禁止向深度缓冲区中写深度值。
+
+		glStencilMask(0x00);
+		glStencilFunc(GL_EQUAL, 1, 0xFF);
+		glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+		//
+		shader.use();
+		glBindVertexArray(VAOId);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textureId);
+		glUniform1i(glGetUniformLocation(shader.programId, "tex"), 0);
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "model"), 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		model = glm::mat4();
+		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(glGetUniformLocation(shader.programId, "model"), 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		//glStencilMask(0xFF);  // 再次允许写入模板缓冲区 以便下次迭代时清除
+		glBindVertexArray(0);
+		glUseProgram(0);
+
+		glfwSwapBuffers(window); // 交换缓存
+	}
+	// 释放资源
+	glDeleteVertexArrays(1, &stencilVAOId);
+	glDeleteBuffers(1, &stencilVBOId);
+	glfwTerminate();
+	return 0;
+}
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE); // 关闭窗口
+	}
+}
