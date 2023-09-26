a091848cb918b3a31e6bc9e10063146d4d83223f
gcy <howareyou_mm@163.com>
2023-09-26 15:38:32 +0800

    将字符串作为一个纹理进行渲染

--- TextRenderBase/TextRenderBase.cpp
+++ TextRenderBase/TextRenderBase.cpp
@@ -12,9 +12,13 @@
 #include <glm/gtc/type_ptr.hpp>
 #include "learnopengl/shader.h"
 #include "learnopengl/vaobuffer.h"
+#include  <codecvt>
 #include "ft2build.h"
 #include FT_FREETYPE_H
-float RenderText( Shader& shader, const string&text, float x, float y, float scale, glm::vec3 color);
+int IntilizeAscIIText();
+int IntilizeUnicodeText(string& text);
+float RenderText(Shader& shader, const string&text,   float x, float y, float scale, glm::vec3 color);
+void RenderText(Shader& shader, const wstring& text, float x, float y, float scale, glm::vec3 color);
 // 键盘回调函数原型声明
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 GLuint vaoId{};
@@ -72,57 +76,9 @@ int main(int argc, char** argv)
 
 	// 设置视口参数
 	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-	FT_Library ft;
-	if (FT_Init_FreeType(&ft))
-	{
-		return -1;
-	}
-	FT_Face face;
-	if (FT_New_Face(ft, "../resources/fonts/arial.ttf",0, &face))
-	{
-		return -1;
-	}
-	FT_Set_Pixel_Sizes(face, 0, 48);
-	//禁用字节对齐限制
-	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-	//准备128个字符的纹理数据
-	for (GLubyte c = 0; c < 128; c++)
-	{
-		//加载字符对应的字形
-		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
-		{
-			std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
-			continue;
-		}
-		//生成纹理
-		GLuint textureId;
-		glGenTextures(1, &textureId);
-		glBindTexture(GL_TEXTURE_2D, textureId);
-		glTexImage2D(
-			GL_TEXTURE_2D, 
-			0, 
-			GL_RED,
-			face->glyph->bitmap.width,
-			face->glyph->bitmap.rows,
-			0,
-			GL_RED,
-			GL_UNSIGNED_BYTE,
-			face->glyph->bitmap.buffer);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		Character character = {
-			textureId,
-			glm::ivec2(face->glyph->bitmap.width,face->glyph->bitmap.rows),
-			glm::ivec2(face->glyph->bitmap_left,face->glyph->bitmap_top),
-			face->glyph->advance.x
-		};
-		mapCharacter[c] = character;
-		glBindTexture(GL_TEXTURE_2D, 0);
-	}
-	FT_Done_Face(face);
-	FT_Done_FreeType(ft);
+	//IntilizeAscIIText();
+	string str("hahhaaa");
+	IntilizeUnicodeText(str);
 	// Section1 准备顶点数据
 	// 指定顶点属性数据 顶点位置
 	GLfloat vertices[24] = {
@@ -162,22 +118,16 @@ int main(int argc, char** argv)
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//// 这里填写场景绘制代码
-		//glBindVertexArray(vaoId);
-		//shader.use();
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-
-		//glBindVertexArray(0);
-		//glUseProgram(0);
-		string str = "Hello World!";
-		//glm::vec3(0.5f, 0.8f, 0.2f)
+		//string str = "Hello World!";
 		glm::vec3 color(0.5f,0.5f,0.5f);
-		float y = 25.f;
+	/*	float y = 25.f;
 		for (int i = 0; i < 5; i++)
 		{
 			float height = RenderText(shader, str, 25, y, 1.0f, color);
 			y += height*1.2f;
-		}
+		}*/
+		wstring strtext = L"我是中国人";
+		RenderText(shader, strtext, 25, 25, 1.0f, color);
 		glfwSwapBuffers(window); // 交换缓存
 	}
 	// 释放资源
@@ -186,53 +136,262 @@ int main(int argc, char** argv)
 	glfwTerminate();
 	return 0;
 }
-// render line of text
-// -------------------
-//void RenderText(Shader &shader, const std::string& text, float x, float y, float scale, glm::vec3 color)
-//{
-//	// activate corresponding render state	
-//	shader.use();
-//	glUniform3f(glGetUniformLocation(shader.ID, "textColor"), color.x, color.y, color.z);
-//	glActiveTexture(GL_TEXTURE0);
-//	glBindVertexArray(vaoId);
-//
-//	// iterate through all characters
-//	std::string::const_iterator c;
-//	for (c = text.begin(); c != text.end(); c++)
-//	{
-//		Character ch = mapCharacter[*c];
-//
-//		float xpos = x + ch.Bearing.x * scale;
-//		float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
-//
-//		float w = ch.Size.x * scale;
-//		float h = ch.Size.y * scale;
-//		// update VBO for each character
-//		float vertices[6][4] = {
-//			{ xpos,     ypos + h,   0.0f, 0.0f },
-//			{ xpos,     ypos,       0.0f, 1.0f },
-//			{ xpos + w, ypos,       1.0f, 1.0f },
-//
-//			{ xpos,     ypos + h,   0.0f, 0.0f },
-//			{ xpos + w, ypos,       1.0f, 1.0f },
-//			{ xpos + w, ypos + h,   1.0f, 0.0f }
-//		};
-//		// render glyph texture over quad
-//		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
-//		// update content of VBO memory
-//		glBindBuffer(GL_ARRAY_BUFFER, vboId);
-//		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // be sure to use glBufferSubData and not glBufferData
-//
-//		glBindBuffer(GL_ARRAY_BUFFER, 0);
-//		// render quad
-//		glDrawArrays(GL_TRIANGLES, 0, 6);
-//		// now advance cursors for next glyph (note that advance is number of 1/64 pixels)
-//		x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
-//	}
-//	glBindVertexArray(0);
-//	glBindTexture(GL_TEXTURE_2D, 0);
-//}
 
+void RenderText(Shader& shader, const wstring& text, float x, float y, float scale, glm::vec3 color)
+{
+	glBindVertexArray(vaoId);
+	glBindBuffer(GL_ARRAY_BUFFER, vboId);
+	//初始化VBO		
+	//int scale = 1.0;
+	Character ch =mapCharacter[10000];
+	float xpos = 0;
+	float ypos = 0;
+	float w = ch.Size.x * scale;
+	float h = ch.Size.y * scale;
+	// update VBO for each character
+	float vertices[6][4] = {
+		{ xpos,     ypos + h,   0.0f, 0.0f },
+		{ xpos,     ypos,       0.0f, 1.0f },
+		{ xpos + w, ypos,       1.0f, 1.0f },
+
+		{ xpos,     ypos + h,   0.0f, 0.0f },
+		{ xpos + w, ypos,       1.0f, 1.0f },
+		{ xpos + w, ypos + h,   1.0f, 0.0f }
+	};
+
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	
+	glBindVertexArray(vaoId);
+	glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+	glBindBuffer(GL_ARRAY_BUFFER, vboId);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); //此处把sizeof(vertices) 写成 size(vertices) 调试了好久没有找到问题
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glBindVertexArray(0);
+}
+int IntilizeAscIIText()
+{
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft))
+	{
+		return -1;
+	}
+	FT_Face face;
+	if (FT_New_Face(ft, "../resources/fonts/arial.ttf", 0, &face))
+	{
+		return -1;
+	}
+	FT_Set_Pixel_Sizes(face, 0, 48);
+	//禁用字节对齐限制
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	//准备128个字符的纹理数据
+	for (GLubyte c = 0; c < 128; c++)
+	{
+		//加载字符对应的字形
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+		{
+			std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+			continue;
+		}
+		//生成纹理
+		GLuint textureId;
+		glGenTextures(1, &textureId);
+		glBindTexture(GL_TEXTURE_2D, textureId);
+		glTexImage2D(
+			GL_TEXTURE_2D,
+			0,
+			GL_RED,
+			face->glyph->bitmap.width,
+			face->glyph->bitmap.rows,
+			0,
+			GL_RED,
+			GL_UNSIGNED_BYTE,
+			face->glyph->bitmap.buffer);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		Character character = {
+			textureId,
+			glm::ivec2(face->glyph->bitmap.width,face->glyph->bitmap.rows),
+			glm::ivec2(face->glyph->bitmap_left,face->glyph->bitmap_top),
+			face->glyph->advance.x
+		};
+		mapCharacter[c] = character;
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
+}
+
+int IntilizeUnicodeText(string& text)
+{
+	int ret = -1;
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft))
+	{
+		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+	}
+
+	FT_Face face;
+	if (FT_New_Face(ft, "../resources/fonts/arial.ttf", 0, &face)) 
+	{
+		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+	}
+	//FT_Set_Charmap(face,FT_Get_System_Charmap());
+
+	FT_Set_Pixel_Sizes(face, 0, 40);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	FT_Vector ftVector;
+	FT_UInt index = -1;
+	ftVector.x = 0;
+	ftVector.y = 0;
+	int minX = (1<<16)-1;
+	int maxX = 0;
+	int minY = (1<<16)-1;
+	int maxY = 0;
+	int PPY = 0;
+	//SIZE
+	int i = 0;
+	bool Isfirst = true;
+	for (auto it = text.begin(); it != text.end(); it++)
+	{
+		auto c = *it;
+
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+		{
+			std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+			continue;
+		}
+
+		index = FT_Get_Char_Index(face, c);
+
+		FT_Load_Glyph(face, index, FT_LOAD_RENDER);
+
+		FT_Bitmap* bitmap = &face->glyph->bitmap;
+
+		auto Py = face->glyph->bitmap.rows - face->glyph->bitmap_top;
+		for (int y = bitmap->rows - 1; y >= 0; --y) 
+		{
+
+			for (int x = 0; x < bitmap->width; x++)
+			{
+
+				int tx = ftVector.x + face->glyph->bitmap_left + x;
+				int ty = ftVector.y + Py - (face->glyph->bitmap.rows - y);
+
+				if (Isfirst)
+				{
+					Isfirst = false;
+					minX = tx;
+					maxX = tx;
+					minY = ty;
+					maxY = ty;
+					PPY = Py;
+				}
+				else
+				{
+					maxX = tx;
+					if (maxY < ty)
+					{
+						maxY = ty;
+					}
+					if (minY > ty)
+					{
+						minY = ty;
+					}
+					if (PPY < Py)
+					{
+						PPY = Py;
+					}
+				}
+
+			}
+		}
+
+		ftVector.x += (face->glyph->advance.x >> 6);
+	}
+	//计算字符串的高度与长度
+	glm::vec2 size(maxX - minX, maxY - minY);
+	//M3D::Math::Vector2 SIZE(MaxX - MinX, MaxY - MinY);
+	size.x += 20;
+	size.y += 20;
+
+	//TEX
+	int texWidth = size.x;
+	int texHeight = size.y;
+	unsigned char* texData = new unsigned char[texWidth * texHeight];
+	memset(texData, 0, texWidth * texHeight);
+	//
+	FT_UInt glyphIndex;
+	FT_Vector pen;
+	pen.x = 10;
+	pen.y = texHeight - 10 - PPY;
+	i = 0;
+	for (auto it = text.begin(); it != text.end(); it++)
+	{
+		if (FT_Load_Char(face, *it, FT_LOAD_RENDER))
+		{
+			std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+			continue;
+		}
+		auto c = *it;
+		glyphIndex = FT_Get_Char_Index(face, c);
+		FT_Load_Glyph(face, glyphIndex, FT_LOAD_RENDER);
+		FT_Bitmap* bitmap = &face->glyph->bitmap;
+		auto Py = face->glyph->bitmap.rows - face->glyph->bitmap_top;
+		for (int y = bitmap->rows - 1; y >= 0; --y) {
+
+			for (int x = 0; x < bitmap->width; x++)
+			{
+				int tx = pen.x + face->glyph->bitmap_left + x;
+				int ty = pen.y + Py - (face->glyph->bitmap.rows - y);
+				int idx = ty * texWidth + tx;
+				texData[idx] = bitmap->buffer[y * bitmap->width + x];
+
+			}
+		}
+		pen.x += (face->glyph->advance.x >> 6);
+	}
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	unsigned int texture2;
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	glTexImage2D(
+		GL_TEXTURE_2D,
+		0,
+		GL_RED,
+		texWidth,
+		texHeight,
+		0,
+		GL_RED,
+		GL_UNSIGNED_BYTE,
+		texData
+	);
+	// set texture options
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	Character character = {
+				texture2,
+				glm::vec2(texWidth ,texHeight),
+				glm::vec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+				static_cast<unsigned int>(face->glyph->advance.x)
+	};
+	mapCharacter.insert(std::pair<unsigned int, Character>(10000, character));
+
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
+	delete[]texData;
+	texData = nullptr;
+	ret = 0;
+	return ret;
+}
 float RenderText( Shader& shader, const string&text, float x, float y, float scale, glm::vec3 color)
 {
 	glBindVertexArray(vaoId);
