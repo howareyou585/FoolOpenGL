78d22e8a9eaaa798863488215816c3154b6c057d
gonggu <howareyou_mm@163.com>
2023-10-04 20:22:11 +0800

    数组纹理

--- TextureArrayDemo/TextureArrayDemo.cpp
+++ TextureArrayDemo/TextureArrayDemo.cpp
@@ -5,21 +5,32 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <vector>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-// 包含着色器加载库
-#include "learnopengl/shader.h"
 
+// 包含着色器加载库
+#include <glm/glm.hpp>
+#include "learnopengl/shader.h"
+#include "learnopengl/vaoBuffer.h"
+#include "learnopengl/vertexset.h"
+#include "learnopengl/boundingbox.h"
+#include "learnopengl/model.h"
+#include "learnopengl/camera.h"
 // 键盘回调函数原型声明
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+//void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void processInput(GLFWwindow *window, Camera & camera);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 // 定义程序常量
 const int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
-
+float deltaTime = 0.0f; // 当前帧与上一帧的时间差
+float lastFrame = 0.0f; // 上一帧的时间
+float lastX = WINDOW_WIDTH / 2.0f;
+float lastY = WINDOW_HEIGHT / 2.0f;
+bool  bFirstMove = true;
+Camera camera;
 int main(int argc, char** argv)
 {
-	
+
 	if (!glfwInit())	// 初始化glfw库
 	{
 		std::cout << "Error::GLFW could not initialize GLFW!" << std::endl;
@@ -46,14 +57,24 @@ int main(int argc, char** argv)
 	glfwMakeContextCurrent(window);
 
 	// 注册窗口键盘事件回调函数
-	glfwSetKeyCallback(window, key_callback);
-
+	//glfwSetKeyCallback(window, key_callback);
+	// 注册窗口鼠标事件回调函数
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	//首先我们要告诉GLFW，它应该隐藏光标，并捕捉(Capture)它。
+	//捕捉光标表示的是，如果焦点在你的程序上
+	//（译注：即表示你正在操作这个程序，Windows中拥有焦点的程序标题栏通常是有颜色的那个，
+	//而失去焦点的程序标题栏则是灰色的），光标应该停留在窗口中（除非程序失去焦点或者退出）。
+	//我们可以用一个简单地配置调用来完成：
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	//在调用这个函数之后，无论我们怎么去移动鼠标，光标都不会显示了，
+	// 它也不会离开窗口。对于FPS摄像机系统来说非常完美。
 	// 初始化GLEW 获取OpenGL函数
 	glewExperimental = GL_TRUE; // 让glew获取所有拓展函数
 	GLenum status = glewInit();
 	if (status != GLEW_OK)
 	{
-		std::cout << "Error::GLEW glew version:" << glewGetString(GLEW_VERSION) 
+		std::cout << "Error::GLEW glew version:" << glewGetString(GLEW_VERSION)
 			<< " error string:" << glewGetErrorString(status) << std::endl;
 		glfwTerminate();
 		return -1;
@@ -61,47 +82,108 @@ int main(int argc, char** argv)
 
 	// 设置视口参数
 	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	VAOBuffer vaoBuffer;
+	vector<vertex_attribute> vecAttrib;
+	map<vertex_attribute, int> mapAttrib2Size;
+	vecAttrib.emplace_back(vertex_attribute::position);
+	vecAttrib.emplace_back(vertex_attribute::texcoord);
+	mapAttrib2Size[vertex_attribute::position] = 3;
+	mapAttrib2Size[vertex_attribute::texcoord] = 2;
+	vaoBuffer.BuildVAO(cubeVertices3, sizeof(cubeVertices3), nullptr,
+		0, vecAttrib, mapAttrib2Size);
 
-	// Section1 准备顶点数据
-	// 指定顶点属性数据 顶点位置
-	GLfloat vertices[] = {
-		-0.5f, 0.0f, 0.0f,
-		0.5f, 0.0f, 0.0f,
-		0.0f, 0.5f, 0.0f
-	};
 	// 创建缓存对象
-	GLuint VAOId, VBOId;
-	// Step1: 创建并绑定VAO对象
-	glGenVertexArrays(1, &VAOId);
-	glBindVertexArray(VAOId);
-	// Step2: 创建并绑定VBO对象
-	glGenBuffers(1, &VBOId);
-	glBindBuffer(GL_ARRAY_BUFFER, VBOId);
-	// Step3: 分配空间 传送数据
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	// Step4: 指定解析方式  并启用顶点属性
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+	GLuint VAOId = vaoBuffer.GetVAO();
+	GLuint VBOId = vaoBuffer.GetVBO();
+	//加载材质
+	vector<string> vecFile;
+	vecFile.emplace_back(string("bricks2.jpg"));
+	vecFile.emplace_back(string("bricks2_disp.jpg"));
+	GLuint texId = TextureArrayFromFile(vecFile, "../resources/textures");
+
+	// world space positions of our cubes
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+	BoundingBox box;
+	int nVal = sizeof(cubeVertices3) / sizeof(GLfloat);
+	for (int i = 0; i < nVal; i += 5)
+	{
+		glm::vec3 pnt(cubeVertices3[i], cubeVertices3[i + 1], cubeVertices3[i + 2]);
+		box.Merge(pnt);
+	}
+	BoundingBox totalBoundingBox;
+	int nModelMatrix = sizeof(cubePositions) / sizeof(glm::vec3);
+	vector<glm::mat4> vecModelMatrix;
+	vecModelMatrix.reserve(nModelMatrix);
+	for (int i = 0; i < nModelMatrix; i++)
+	{
+		glm::mat4 model(1.0f);
+		model = glm::translate(model, cubePositions[i]);
+		float angle = 20.0f * i;
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+		BoundingBox tmpBoundingBox = box.Transformed(model);
+		totalBoundingBox.Merge(tmpBoundingBox);
+		vecModelMatrix.emplace_back(model);
+	}
+	//float raduis = totalBoundingBox.GetLength()*0.8f;
+
+	camera.InitCamera(totalBoundingBox, 0.8f);
+	float radius = totalBoundingBox.GetLength() * 0.8f;
 
 	// Section2 准备着色器程序
 	Shader shader("TextureArrayDemo.vertex", "TextureArrayDemo.frag");
+	shader.use();
+	shader.setInt("textureArray", 0);
 
+	shader.unUse();
+	int nVertex = sizeof(cubeVertices3) / (sizeof(GLuint) * 5);
 	// 开始游戏主循环
+	glEnable(GL_DEPTH_TEST);
+	//glCullFace(GL_BACK);
+	glm::vec3 targetPos = totalBoundingBox.GetCenter();
+	float distance = glm::length(targetPos - camera.Position);
 	while (!glfwWindowShouldClose(window))
 	{
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame; // 上一帧的时间
+		processInput(window, camera);
 		glfwPollEvents(); // 处理例如鼠标 键盘等事件
 
 		// 清除颜色缓冲区 重置为指定颜色
 		glClearColor(0.18f, 0.04f, 0.14f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// 这里填写场景绘制代码
 		glBindVertexArray(VAOId);
 		shader.use();
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//鼠标移动，镜头方向不变
+		targetPos = camera.Position + distance * camera.Front;
+		glm::mat4 viewMatrix = camera.GetViewMatrix(targetPos);
 
+		shader.setMat4("view", viewMatrix);
+		glm::mat4 projectionMatrix = camera.GetProjectionMatrix((float)WINDOW_WIDTH / (float)WINDOW_HEIGHT);
+		shader.setMat4("projection", projectionMatrix);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D_ARRAY, texId);
+	
+		for (auto j = 0; j < nModelMatrix; j++)
+		{
+			shader.setMat4("model", vecModelMatrix[j]);
+			glDrawArrays(GL_TRIANGLES, 0, nVertex);
+			//glDrawArrays(GL_LINES, 0, nVertex);
+		}
+		shader.unUse();
 		glBindVertexArray(0);
 		glUseProgram(0);
 
@@ -113,10 +195,76 @@ int main(int argc, char** argv)
 	glfwTerminate();
 	return 0;
 }
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+//void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+//{
+//	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+//	{
+//		glfwSetWindowShouldClose(window, GL_TRUE); // 关闭窗口
+//	}
+//}
+void processInput(GLFWwindow *ptrWindow, Camera & camera)
 {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	if (!ptrWindow)
 	{
-		glfwSetWindowShouldClose(window, GL_TRUE); // 关闭窗口
+		return;
 	}
+	Camera_Movement direction;
+	bool bMove = false;
+	if (glfwGetKey(ptrWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(ptrWindow, true);
+
+	}
+	else if (glfwGetKey(ptrWindow, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		direction = Camera_Movement::FORWARD;
+		bMove = true;
+		//camera.ProcessKeyboard()
+	}
+	else if (glfwGetKey(ptrWindow, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		direction = Camera_Movement::BACKWARD;
+		bMove = true;
+		//camera.ProcessKeyboard()
+	}
+	else if (glfwGetKey(ptrWindow, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		direction = Camera_Movement::LEFT;
+		bMove = true;
+		//camera.ProcessKeyboard()
+	}
+	else if (glfwGetKey(ptrWindow, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		direction = Camera_Movement::RIGHT;
+		bMove = true;
+		//camera.ProcessKeyboard()
+	}
+	if (bMove)
+	{
+		camera.ProcessKeyboard(direction, deltaTime);
+	}
+}
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+	if (bFirstMove)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		bFirstMove = false;
+	}
+
+	float offsetX = xpos - lastX;
+	float offsetY = lastY - ypos; // y 坐标往下越来越大。
+	//std::cout << "lastY="<<lastY <<",ypos =" <<ypos<< ",offsetY =" <<offsetY << endl;
+	//std::cout << "offsetX=" << offsetX << ",offsetY=" << offsetY << std::endl;
+	lastX = xpos;
+	lastY = ypos;
+	camera.ProcessMouseMovement(offsetX, offsetY);
+}
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	//std::cout << "xoffset=" << xoffset << ",yoffset=" << yoffset << std::endl;
+	camera.ProcessMouseScroll(yoffset);
 }
--- TextureArrayDemo/TextureArrayDemo.frag
+++ TextureArrayDemo/TextureArrayDemo.frag
@@ -1,8 +1,10 @@
 #version 330
 
-out vec4 color;
-
+out vec4 FragColor;
+in vec2 texCoord;
+uniform sampler2DArray textureArray;
 void main()
 {
-	color = vec4(0.8, 0.8, 0.0, 1.0);
+	
+	FragColor = texture(textureArray, vec3(texCoord,1));
 }
